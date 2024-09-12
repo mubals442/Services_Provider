f99490c4f6dec2832ae41da2fa084e3ddec17088
Musab Alsayed <musab20151@gmail.com>
2024-09-12 20:16:47 +0300

    initial a structures files

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+
+const useToggle = (initialValue = false) => {
+  const [toggle, setToggle] = useState(initialValue);
+
+  const changeToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return [toggle, changeToggle];
+};
+
+export default useToggle;
